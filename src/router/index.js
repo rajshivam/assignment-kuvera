1d81d66fde04a8769443877cbeb22d71c31d1ad2
Shivam Raj <rajshivam12.5@gmail.com>
2020-07-10 02:53:22 +0530

    router setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Explore from "../views/Explore.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/explore"
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/explore",
+    name: "Explore",
+    component: Explore
+  },
+  {
+    path: "/explore/:fund-code",
+    name: "FundDetail",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/FundDetail.vue")
   }
 ];
 

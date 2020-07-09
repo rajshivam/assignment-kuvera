0ac9eb25121f7657a9ef87afb01d69a9cdccf1b4
Shivam Raj <rajshivam12.5@gmail.com>
2020-07-10 02:41:41 +0530

    added service and state for funds

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import service from "./services/index";
+import "bulma/css/bulma.css";
+
+Vue.use(service);
 
 Vue.config.productionTip = false;
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    funds: []
+  },
+  mutations: {
+    setFunds(state, payload) {
+      state.funds = payload;
+    }
+  },
+  actions: {
+    async loadFunds({ commit }) {
+      try {
+        //get funds from api
+        let result = await Vue.service.getFunds();
+        commit("setFunds", result);
+      } catch (error) {
+        console.log("store loadFunds error", error);
+      }
+    }
+  }
 });

714c945d21142d3b618718e28727cad01484fd20
Shivam Raj <rajshivam12.5@gmail.com>
2020-07-10 05:47:08 +0530

    added fontawesome and bulma

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import service from "./services/index";
+
 import "bulma/css/bulma.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(service);
 
@@ -12,5 +20,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
